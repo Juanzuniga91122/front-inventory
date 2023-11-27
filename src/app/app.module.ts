a8d812fe43a742012c9c45e6c857f56cd4e37e53
juanzuniga <juanzuniga@unicomfacauca.edu.co>
2023-11-26 21:22:23 -0500

    primera version del front-end inventory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/dashboard/pages/dashboard.component';
+import { HomeComponent } from './modules/dashboard/components/home/home.component';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
+/*Se eliminan los componentes: DashboardComponent, HomeComponent  del app.modulo ts por que se agregaron al modulo de dashboard ts */
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
